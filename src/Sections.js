e40635b24961ef71f72851cc4b7315185aae57b1
vicantlop <vicantlop@gmail.com>
2023-06-12 14:37:08 -0700

    added icon for url and adjusted about me page

--- src/Sections.js
+++ src/Sections.js
@@ -1,18 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home';
-import Contact from './components/Contact';
-import About from './components/About';
-import Projects from './components/Projects';
 import NoPage from './components/NoPage';
 
 const Sections = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />}>
-                <Route index element={<Home />} />
-                <Route path="contact" element={<Contact />} />
-                <Route path="about" element={<About />} />
-                <Route path="projects" element={<Projects />} />
                 <Route path="*" element={<NoPage />} />
             </Route>
         </Routes>
--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,17 @@ const About = () => {
             <h1>About Me</h1>
             <p>Hello there! I'm Victor Lopez, a passionate and dedicated Full Stack Software Engineer based in San Diego, CA. With a strong background in JavaScript, Node.js, HTML5, CSS, SQL, React, and Redux, I thrive in creating robust and user-friendly web applications.
 
-                I am a recent graduate of Fullstack Academy's Software Engineering Full-Time Immersive Program, where I gained hands-on experience in developing innovative solutions using modern technologies and following Agile methodologies. My technical projects, Fern Finances and Tavern Minis, highlight my ability to collaborate effectively with teams and deliver exceptional user experiences.
+                I am a recent graduate of Fullstack Academy's Software Engineering Full-Time Immersive Program, where I gained hands-on experience in developing innovative solutions using modern technologies and following Agile methodologies.
+            </p>
+            <p>My technical projects, Fern Finances and Tavern Minis, highlight my ability to collaborate effectively with teams and deliver exceptional user experiences.
 
                 Beyond my technical skills, I bring a unique blend of leadership and revenue generation expertise. As the Founder of Lopez Wrestling, I optimized revenue generation by identifying the target client and implementing effective strategies, resulting in significant growth. Additionally, my experience in substitute teaching and insurance analysis has sharpened my communication and analytical skills, allowing me to understand and meet the needs of diverse stakeholders.
 
-                I am passionate about leveraging technology to solve real-world challenges and create meaningful experiences for users. With my solid foundation in both front-end and back-end development, I am ready to contribute to dynamic teams and drive innovation.
+            </p>
+            <p>I am passionate about leveraging technology to solve real-world challenges and create meaningful experiences for users. With my solid foundation in both front-end and back-end development, I am ready to contribute to dynamic teams and drive innovation.
 
-                Let's connect and explore how I can contribute to your organization's success.</p>
+                Let's connect and explore how I can contribute to your organization's success.
+            </p>
         </div>
 
 
